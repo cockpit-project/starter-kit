ccaf563a3ae5a7e4b8fdc097c400383db0ce1b4f
Martin Pitt <martin@piware.de>
2020-12-17 07:36:34 +0100

    webpack: Fix PatternFly font path
    Copy the rule that cockpit and cockpit-podman use. Also change the
    quoting, to avoid pointless diff noise. Now they are perfectly in sync.

--- webpack.config.js
+++ webpack.config.js
@@ -108,7 +108,7 @@ module.exports = {
                         loader: 'css-loader',
                         options: {
                             sourceMap: true,
-                            url: false
+                            url: false,
                         }
                     },
                     {
@@ -117,7 +117,7 @@ module.exports = {
                             multiple: [
                                 {
                                     search: /src:url\("patternfly-icons-fake-path\/pficon[^}]*/g,
-                                    replace: "src:url('fonts/patternfly.woff')format('woff');",
+                                    replace: 'src:url("../base1/fonts/patternfly.woff") format("woff");',
                                 },
                                 {
                                     search: /@font-face[^}]*patternfly-fonts-fake-path[^}]*}/g,
