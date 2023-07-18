39e0f576cad7caaf9f6547a1c1753308696beee5
Martin Pitt <mpitt@redhat.com>
2023-07-18 05:30:11 +0200

    package.json: Update to React 18
    Update to the new root creation function.

--- src/index.js
+++ src/index.js
@@ -21,10 +21,10 @@ import "cockpit-dark-theme";
 import "patternfly/patternfly-5-cockpit.scss";
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Application } from './app.jsx';
 import './app.scss';
 
-document.addEventListener("DOMContentLoaded", function () {
-    ReactDOM.render(React.createElement(Application, {}), document.getElementById('app'));
+document.addEventListener("DOMContentLoaded", () => {
+    createRoot(document.getElementById("app")).render(<Application />);
 });
