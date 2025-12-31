6014787788e30fa2ec3f93b61ce5e7d8cfc358b4
Martin Pitt <mpitt@redhat.com>
2025-12-31 08:26:26 +0100

    build.js: Harmonize with cockpit-podman
    Almost no functional changes (other than fixed error handling in asset
    copying), just some noise that built up over the years.

--- build.js
+++ build.js
@@ -2,18 +2,17 @@
 
 import fs from 'node:fs';
 import { createRequire } from 'node:module';
+import os from 'node:os';
 import path from 'node:path';
 import process from 'node:process';
-import os from 'node:os';
 
 import { sassPlugin } from 'esbuild-sass-plugin';
 
-import { cleanPlugin } from './pkg/lib/esbuild-cleanup-plugin.js';
-import { cockpitCompressPlugin } from './pkg/lib/esbuild-compress-plugin.js';
 import { cockpitPoEsbuildPlugin } from './pkg/lib/cockpit-po-plugin.js';
 import { cockpitRsyncEsbuildPlugin } from './pkg/lib/cockpit-rsync-plugin.js';
+import { cleanPlugin } from './pkg/lib/esbuild-cleanup-plugin.js';
+import { cockpitCompressPlugin } from './pkg/lib/esbuild-compress-plugin.js';
 
-const production = process.env.NODE_ENV === 'production';
 const useWasm = os.arch() !== 'x64';
 
 const esbuild = await (async () => {
@@ -32,20 +31,23 @@ const esbuild = await (async () => {
     }
 })();
 
-const parser = (await import('argparse')).default.ArgumentParser();
-parser.add_argument('-r', '--rsync', { help: "rsync bundles to ssh target after build", metavar: "HOST" });
-parser.add_argument('-w', '--watch', { action: 'store_true', help: "Enable watch mode", default: process.env.ESBUILD_WATCH === "true" });
-const args = parser.parse_args();
-
-if (args.rsync)
-    process.env.RSYNC = args.rsync;
-
+const production = process.env.NODE_ENV === 'production';
 // List of directories to use when using import statements
 const nodePaths = ['pkg/lib'];
 const outdir = 'dist';
 
 // Obtain package name from package.json
 const packageJson = JSON.parse(fs.readFileSync('package.json'));
+
+const parser = (await import('argparse')).default.ArgumentParser();
+/* eslint-disable max-len */
+parser.add_argument('-r', '--rsync', { help: "rsync bundles to ssh target after build", metavar: "HOST" });
+parser.add_argument('-w', '--watch', { action: 'store_true', help: "Enable watch mode", default: process.env.ESBUILD_WATCH === "true" });
+/* eslint-enable max-len */
+const args = parser.parse_args();
+
+if (args.rsync)
+    process.env.RSYNC = args.rsync;
 
 function notifyEndPlugin() {
     return {
@@ -94,25 +96,28 @@ const context = await esbuild.context({
     ...!production ? { sourcemap: "linked" } : {},
     bundle: true,
     entryPoints: ['./src/index.js'],
-    external: ['*.woff', '*.woff2', '*.jpg', '*.svg', '../../assets*'], // Allow external font files which live in ../../static/fonts
-    legalComments: 'external', // Move all legal comments to a .LEGAL.txt file
-    loader: { ".js": "jsx" },
-    metafile: true,
+    // Allow external font files which live in ../../static/fonts
+    external: ['*.woff', '*.woff2', '*.jpg', '*.svg', '../../assets*'],
+    // Move all legal comments to a .LEGAL.txt file
+    legalComments: 'external',
+    loader: { ".js": "jsx", ".py": "text" },
     minify: production,
     nodePaths,
     outdir,
+    metafile: true,
     target: ['es2020'],
     plugins: [
         cleanPlugin(),
-
         // Esbuild will only copy assets that are explicitly imported and used in the code.
         // Copy the other files here.
         {
             name: 'copy-assets',
             setup(build) {
-                build.onEnd(() => {
-                    fs.copyFileSync('./src/manifest.json', './dist/manifest.json');
-                    fs.copyFileSync('./src/index.html', './dist/index.html');
+                build.onEnd((output, _outputFiles) => {
+                    if (output?.errors.length === 0) {
+                        fs.copyFileSync('./src/manifest.json', './dist/manifest.json');
+                        fs.copyFileSync('./src/index.html', './dist/index.html');
+                    }
                 });
             }
         },
